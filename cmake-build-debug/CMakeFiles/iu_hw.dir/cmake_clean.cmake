fe9add3641d213b384ae538ac60904141c402aeb
@Maus_74 <nikita.potapovich@mail.ru>
2019-11-12 23:03:34 +0300

    ferr

---
+++ cmake-build-debug/CMakeFiles/iu_hw.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iu_hw.dir/sources/source.cpp.o"
+  "libiu_hwd.pdb"
+  "libiu_hwd.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iu_hw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/iu_hw.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libiu_hwd.a"
+)
