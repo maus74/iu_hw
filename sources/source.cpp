07079b6cf53c269a42ceca1e5a53844ca68339cc
@Maus_74 <nikita.potapovich@mail.ru>
2019-11-12 12:53:14 +0300

    code

--- sources/source.cpp
+++ sources/source.cpp
@@ -2,9 +2,36 @@
 
 #include <header.hpp>
 
+int sum(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+
+int sub(int a ,int b)
+{
+    int c = a - b;
+    return c;
+}
+
+int mult(int a, int b)
+{
+    int c = a * b;
+    return c;
+}
 int main()
 {
     int n;
-    cin>>n;
-    cout<<n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a == 1)
+            cout<<sum(b,c);
+        if(a==2)
+            cout<<sub(b,c);
+        if(a==3)
+            cout<<mult(b,c);
+    }
 }
+
