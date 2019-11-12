0e3400e9aa26d3581ee674727a7fa825163164b3
@Maus_74 <nikita.potapovich@mail.ru>
2019-11-12 14:13:03 +0300

    end

--- tools/gate/cmake/HunterGate.cmake
+++ tools/gate/cmake/HunterGate.cmake
@@ -1,9 +1,4 @@
 HunterGate(
-<<<<<<< HEAD
         URL "https://github.com/ruslo/hunter/archive/v0.23.214.tar.gz"
         SHA1 "e14bc153a7f16d6a5eeec845fb0283c8fad8c358"
-=======
-    URL "https://github.com/ruslo/hunter/archive/v0.23.214.tar.gz"
-    SHA1 "e14bc153a7f16d6a5eeec845fb0283c8fad8c358"
->>>>>>> ce660b72e76b8ccb206cdf7a0f0eae55ac43ffae
 )
